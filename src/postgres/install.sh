ce91042c514868fb9855dc63e2af921ce20b4bb2
Bear Cherian <bearcherian@github.com>
2023-03-16 04:20:17 +0000

    initial attempt to run postgres

---
+++ src/postgres/install.sh
@@ -0,0 +1,10 @@
+set -e
+
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/pgdg.asc &>/dev/null
+
+apt update
+
+apt install -y postgresql postgresql-contrib
+
+service postgresql start
